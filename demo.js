a12170d30a6d647b63c0440090272ec9e6b7c34a
Marak Squires <marak.squires@gmail.com>
2010-06-12 23:03:52 -0400

    added colors.js, cloudhead owns your face

--- demo.js
+++ demo.js
@@ -1,10 +1,11 @@
 var sys = require('sys');
+var translate = require('./colors'); // colors are fun!
 var translate = require('./translate');
     
 // note: the translator is actually English=>Spanish by default
 translate.text('I want tacos please.', function(text){
 
-  sys.puts('I want tacos please. => ' + text); // outputs : i want tacos please
+  sys.puts('I want tacos please.'.red + ' => '.cyan + text.green); // outputs : i want tacos please
   
   // set the input language to English
   translate.input('Spanish');
@@ -13,7 +14,7 @@ translate.text('I want tacos please.', function(text){
   translate.output('English');
 
   translate.text(text, function(text2){
-    sys.puts(text + ' => ' + text2);
+    sys.puts(text.green + ' => '.cyan + text2.red);
     sys.puts('English To Spanish taco request has been normalized. ^_^');
   }); 
 
