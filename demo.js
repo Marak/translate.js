d130640bdddd23aef3644498b4d193261c67251d
Marak Squires <marak.squires@gmail.com>
2010-06-25 22:27:49 -0400

    update to 0.2.0. improved API. updated docs

--- demo.js
+++ demo.js
@@ -2,32 +2,23 @@ var sys = require('sys');
 var colors = require('./colors'); // colors are fun!
 var translate = require('./translate');
 
-translate.input('English'); // you could omit this line, English=>Spanish is the default
-
-
 // TODO: make this demo automated and based on all languages
     
-// note: the translator is actually English=>Spanish by default
+// note: the translator is  English=>Spanish by default
 translate.text('I want tacos please.', function(text){
 
-  sys.puts('I want tacos please.'.red + ' => '.cyan + text.yellow); // outputs : i want tacos please
-  
-  // set the input language to English
-  translate.input('Spanish');
+  sys.puts('I want tacos please.'.red + ' => '.cyan + text.yellow);
+  var input = 'Spanish', output = "Japanese";
+  translate.text({input:input,output:output}, text, function(text2){
 
-  // set the output language Spanish
-  translate.output('Japanese');
-
-  translate.text(text, function(text2){
+    var input = 'Japanese', output = "English";
     sys.puts(text.yellow + ' => '.cyan + text2.blue);
-    translate.output('English');
-    
-    translate.text(text, function(text3){
+    translate.text({input:input,output:output}, text, function(text3){
+
        sys.puts(text2.blue + ' => '.cyan + text3.red);
        sys.puts('English'.red+'=>'+'Spanish'.yellow+'=>'+'Japanese'.blue+'=>'+'English'.red  +'\ntaco request has been normalized. ^_^'.green);
     });
   }); 
-
 }); 
 
 
--- translate.js
+++ translate.js
@@ -6,32 +6,30 @@ var sys = require('sys')
 var languages = require('./languages').getLangs();
 var translates = exports;
 
-// set the default input and output languages
-var input = languages['English'];
-var output = languages['Spanish'];
-
 // http client for accessing google api
 var googleTranslate = http.createClient(80, 'ajax.googleapis.com');
 
 // simple fn to get the path given text to translate
-var getEnglishTranslatePath = function (text, lang) {
+var getEnglishTranslatePath = function (lang, text) {
+
+  // set the default input and output languages to English and Spanish
+  var input = languages[lang.input] || 'en',
+  output = languages[lang.output] || 'es';
+
   return ['/ajax/services/language/translate?v=1.0&q='
             ,encodeURIComponent(text, lang)
             ,'&langpair=' + input + '|' + output].join("");
 }
 
-exports.input = function(language){
-  input = languages[language];
-};
-exports.output = function(language){
-  output = languages[language];
-};
-
 // translate the text
-exports.text = function (text, callback) {
-  var req = googleTranslate.request('GET', getEnglishTranslatePath(text),
+exports.text = function (lang, text, callback) {
+  // this is not a good curry recipe. needs moar spice
+  if(typeof lang !== 'object'){
+    callback = text;
+    text = lang;
+  }
+  var req = googleTranslate.request('GET', getEnglishTranslatePath(lang, text),
     {'host': 'ajax.googleapis.com', 'encoding':'utf-8'});
-    
   req.addListener('response', function (response) {
     var responseBody = "";
     response.addListener('data', function (chunk) {
