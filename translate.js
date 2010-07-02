d550b8290f334d6e874c281174a73a235dec1afd
Marak Squires <marak.squires@gmail.com>
2010-07-01 21:34:38 -0400

    dualing it up

--- translate.js
+++ translate.js
@@ -1,13 +1,5 @@
 // Andrew Lunny and Marak Squires
 // Mit yo, copy paste us some credit
-var sys = require('sys')
-  , http = require('http');
-  
-var languages = require('./languages').getLangs();
-var translates = exports;
-
-// http client for accessing google api
-var googleTranslate = http.createClient(80, 'ajax.googleapis.com');
 
 // simple fn to get the path given text to translate
 var getEnglishTranslatePath = function (lang, text) {
@@ -21,28 +13,51 @@ var getEnglishTranslatePath = function (lang, text) {
             ,'&langpair=' + input + '|' + output].join("");
 }
 
-// translate the text
-exports.text = function (lang, text, callback) {
-  // this is not a good curry recipe. needs moar spice
-  if(typeof lang !== 'object'){
-    callback = text;
-    text = lang;
+// stupid if else statement, i felt like actually making his library dual-sided instead of bashing my head against gemini.js 
+if(typeof exports === 'undefined'){
+
+  
+
+  
+}
+else{
+
+  var sys = require('sys')
+    , http = require('http');
+
+  var languages = require('./languages').getLangs();
+  var translates = exports;
+
+  // http client for accessing google api
+  var googleTranslate = http.createClient(80, 'ajax.googleapis.com');
+
+  // translate the text
+  exports.text = function (lang, text, callback) {
+    // this is not a good curry recipe. needs moar spice
+    if(typeof lang !== 'object'){
+      callback = text;
+      text = lang;
+    }
+    var req = googleTranslate.request('GET', getEnglishTranslatePath(lang, text),
+      {'host': 'ajax.googleapis.com', 'encoding':'utf-8'});
+    req.addListener('response', function (response) {
+      var responseBody = "";
+      response.addListener('data', function (chunk) {
+        responseBody += chunk;
+      });
+      response.addListener('end', function () {
+        var bodyObj = JSON.parse(responseBody);
+        if (bodyObj.responseStatus === 200) {
+          callback(bodyObj.responseData.translatedText);
+        } else {
+          sys.debug("Translate API call failed");
+        }
+      });
+    });
+    req.end();
   }
-  var req = googleTranslate.request('GET', getEnglishTranslatePath(lang, text),
-    {'host': 'ajax.googleapis.com', 'encoding':'utf-8'});
-  req.addListener('response', function (response) {
-    var responseBody = "";
-    response.addListener('data', function (chunk) {
-      responseBody += chunk;
-    });
-    response.addListener('end', function () {
-      var bodyObj = JSON.parse(responseBody);
-      if (bodyObj.responseStatus === 200) {
-        callback(bodyObj.responseData.translatedText);
-      } else {
-        sys.debug("Translate API call failed");
-      }
-    });
-  });
-  req.end();
+
+
+  
+  
 }
